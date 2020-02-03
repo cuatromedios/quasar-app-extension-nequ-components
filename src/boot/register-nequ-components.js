c3d7bbce6346744440d1486e079efc59b46932eb
Ramses <ramses@cuatromedios.com>
2020-02-03 14:24:48 -0600

    Including a generic input to test inheritance of slots

--- src/boot/register-nequ-components.js
+++ src/boot/register-nequ-components.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import NqCurrency from '../components/NqCurrency.vue'
+import NqInput from '../components/NqInput.vue'
 
 // we globally register our component
 Vue.component('nq-currency', NqCurrency)
+Vue.component('nq-input', NqInput)
---
+++ src/components/NqInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-input :outlined="!filled && !standout && !borderless"
+           class="nq-input"
+           v-bind="$attrs"
+           v-on="$listeners">
+  </q-input>
+</template>
+
+<script>
+  export default {
+    name: 'NqInput',
+    props: {
+      filled:Boolean,
+      standout:Boolean,
+      borderless:Boolean
+    }
+  }
+</script>
