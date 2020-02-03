e7f177b93ca77a3d00dafe84417c58d30e460805
Ramses <ramses@cuatromedios.com>
2020-02-03 14:32:36 -0600

    Slot inheritance in generic input

--- src/components/NqInput.vue
+++ src/components/NqInput.vue
@@ -3,6 +3,7 @@
            class="nq-input"
            v-bind="$attrs"
            v-on="$listeners">
+    <template v-for="(_, slot) of $scopedSlots" v-slot:[slot]="scope"><slot :name="slot" v-bind="scope"/></template>
   </q-input>
 </template>
 
