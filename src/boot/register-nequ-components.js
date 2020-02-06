4b4ce896f803ad337a7562abe885b1c6713cee08
Ramses <ramses@cuatromedios.com>
2020-02-06 08:20:57 -0600

    NqField

--- src/boot/register-nequ-components.js
+++ src/boot/register-nequ-components.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import NqField from '../components/NqField.vue'
 import NqInput from '../components/NqInput.vue'
 import NqInputNumber from '../components/NqInputNumber.vue'
 import NqInputCurrency from '../components/NqInputCurrency.vue'
@@ -6,6 +7,7 @@ import NqInputPercentage from '../components/NqInputPercentage.vue'
 import NqSelect from '../components/NqSelect'
 
 // we globally register our component
+Vue.component('nq-field', NqField)
 Vue.component('nq-input', NqInput)
 Vue.component('nq-input-number', NqInputNumber)
 Vue.component('nq-input-currency', NqInputCurrency)
---
+++ src/components/NqField.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-field v-bind="$attrs"
+           v-on="$listeners"
+           v-on:focus="onFocus"
+           v-on:blur="onBlur"
+           :outlined="!filled && !standout && !borderless"
+           class="nq-field nq-component"
+           >
+    <template v-for="(_, slot) of $scopedSlots" v-slot:[slot]="scope"><slot :name="slot" v-bind="scope"/></template>
+  </q-field>
+</template>
+
+<script>
+  import {field} from '../mixins/field'
+
+  export default {
+    name: 'NqField',
+    mixins: [ field ],
+    props: {
+      filled:Boolean,
+      standout:Boolean,
+      borderless:Boolean
+    },
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
