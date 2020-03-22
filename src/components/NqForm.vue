4f1130e29a4a266cfea7b9371d9603b96de3dd1d
Ramses <ramses@cuatromedios.com>
2020-03-21 18:46:55 -0600

    Form refactored: Less space. Always inside a card.

--- src/components/NqForm.vue
+++ src/components/NqForm.vue
@@ -4,19 +4,26 @@
           greedy
           :disabled="!ready || submitting"
   >
-    <div v-if="ready" class="row q-col-gutter-sm">
-      <slot name="default"></slot>
-      <div class="flex flex-center col-12">
-        <q-btn v-if="cancelLabel !== ''" :label="cancelLabel" size="md" color="primary" flat class="q-mr-lg" @click="onCancel" />
-        <q-btn :label="submitLabel" size="lg" push type="submit" color="primary" :loading="submitting"/>
-      </div>
-    </div>
-    <div v-if="!ready" class="row col-12">
-      <q-skeleton class="col-12 q-mb-md" type="QInput" height="48px"  />
-      <q-skeleton class="col-12 q-mb-md" type="QInput" height="48px"  />
-      <q-skeleton class="offset-4 col-2 q-mr-md" type="QBtn" height="48px"  />
-      <q-skeleton class="col-2" type="QBtn" height="48px"  />
-    </div>
+    <q-card>
+      <q-card-section>
+          <div v-if="ready" class="row q-col-gutter-y-sm q-col-gutter-x-lg">
+            <slot name="default"></slot>
+          </div>
+          <div v-if="!ready" class="row col-12">
+            <q-skeleton class="col-12 q-mb-md" type="QInput" height="48px"  />
+            <q-skeleton class="col-12 q-mb-md" type="QInput" height="48px"  />
+            <q-skeleton class="offset-4 col-2 q-mr-md" type="QBtn" height="48px"  />
+            <q-skeleton class="col-2" type="QBtn" height="48px"  />
+          </div>
+      </q-card-section>
+      <q-separator />
+      <q-card-actions class="q-pa-md bg-grey-2">
+        <div class="flex flex-center col-12">
+          <q-btn v-if="cancelLabel !== ''" :label="cancelLabel" size="md" color="primary" flat class="q-mr-lg" @click="onCancel" />
+          <q-btn :label="submitLabel" size="lg" push type="submit" color="primary" :loading="submitting"/>
+        </div>
+      </q-card-actions>
+    </q-card>
   </q-form>
 </template>
 
