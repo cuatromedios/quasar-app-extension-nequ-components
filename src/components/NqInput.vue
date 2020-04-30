33bb749965b0885d646d7834bb64e4938465488b
Ramses Moreno <ramses@cuatromedios.com>
2020-04-30 11:51:37 -0500

    Allow to NqInput to receive a size attribute

--- src/components/NqInput.vue
+++ src/components/NqInput.vue
@@ -7,7 +7,8 @@
            :required="false"
            :outlined="!filled && !standout && !borderless"
            class="nq-input nq-component"
-           :input-class="`${$attrs['input-class'] || ''} nq-input-field`"
+           :dense="size==='sm' || size==='xs' || $attrs.dense"
+           :input-class="`nq-input-field ${$attrs['input-class'] || ''} size-${size}`"
            >
     <template v-for="(_, slot) of $scopedSlots" v-slot:[slot]="scope"><slot :name="slot" v-bind="scope"/></template>
   </q-input>
@@ -22,10 +23,34 @@
     props: {
       filled:Boolean,
       standout:Boolean,
-      borderless:Boolean
+      borderless:Boolean,
+      size: {
+        type: String,
+        default: 'md'
+      }
     },
     data () {
       return {}
     }
   }
 </script>
+<style lang="scss">
+  .nq-input-field.size-xs {
+    font-size: 0.75em;
+  }
+  .nq-input-field.size-sm {
+    font-size: 1em;
+  }
+  .nq-input-field.size-md {
+    font-size: 1.25em;
+  }
+  .nq-input-field.size-lg {
+    font-size: 1.33em;
+  }
+  .nq-input-field.size-xl {
+    font-size: 1.75em;
+  }
+  textarea.nq-input-field.q-field__native {
+    line-height: 120%;
+  }
+</style>
